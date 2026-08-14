dc04fd90db8082406ed3db0c564734d82d0e1986
Andre LaFleur <cincospenguinos@gmail.com>
2026-08-14 14:36:13 -0600

    I can draw a rectangle

--- src/c/CantRead.c
+++ src/c/CantRead.c
@@ -1,43 +1,35 @@
 #include <pebble.h>
 
 static Window *s_window;
-static TextLayer *s_text_layer;
+static Layer *s_canvas_layer;
 
-static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Select");
-}
+static void canvas_update_proc(Layer *layer, GContext *ctx) {
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+  graphics_context_set_fill_color(ctx, GColorBlack);
 
-static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Up");
-}
-
-static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Down");
-}
-
-static void prv_click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
+  // Okay, we're able to draw!
+  GRect bounds = GRect(0, 0, 25, 19);
+  graphics_draw_rect(ctx, bounds);
+  graphics_fill_rect(ctx, bounds, 0, GCornersAll);
 }
 
 static void prv_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(s_text_layer, "Press a button");
-  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+  s_canvas_layer = layer_create(bounds);
+  layer_set_update_proc(s_canvas_layer, canvas_update_proc);
+  layer_add_child(window_get_root_layer(window), s_canvas_layer);
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done loading window; (%d, %d), %d x %d", bounds.origin.x, bounds.origin.y, bounds.size.w, bounds.size.h);
 }
 
 static void prv_window_unload(Window *window) {
-  text_layer_destroy(s_text_layer);
+  layer_destroy(s_canvas_layer);
 }
 
 static void prv_init(void) {
   s_window = window_create();
-  window_set_click_config_provider(s_window, prv_click_config_provider);
   window_set_window_handlers(s_window, (WindowHandlers) {
     .load = prv_window_load,
     .unload = prv_window_unload,
